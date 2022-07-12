5ee1bfa3d3fcd8dc2959abd88d5f67e062d3a25e
slavontm <nslava@gmail.com>
2022-07-12 16:16:57 +0300

    first commit

---
+++ 1hellow/src/HelloW.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class HelloW {
+public static void main(String[] args) {
+
+
+    String Data=new Date().toString();
+
+    System.out.println("Hello World! "+ Data);
+// Add date;
+
+}
+
+}
---
+++ 2Variables/src/Varables.java
@@ -0,0 +1,28 @@
+public class Varables{
+    public static void main (String[] args)
+
+    {
+        int myInt=557;
+        short myShort=25;
+        long myLong=12345678L; // Long - no default int
+
+        double myDouble=3.45546;
+        float myFloat=3.56f; // float - no default double
+
+        char c='b';
+        boolean bool=true;
+
+        byte myByte=-14; //-128 127
+
+
+        System.out.println(myInt);
+        System.out.println(myShort);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(c);
+        System.out.println(bool);
+        System.out.println(myByte);
+    }
+
+
+}
