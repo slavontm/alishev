00781faeb42a795ada4f97773624f7d99f1f8a6f
slavontm <nslava@gmail.com>
2022-07-13 16:54:08 +0300

    new

---
+++ 6If/src/If.java
@@ -0,0 +1,16 @@
+public class If {
+    public static void main(String[] args) {
+
+        int myInt=15;
+
+        if (myInt<15) {
+            System.out.println("myInt<15");
+        }
+        else if (myInt>15){
+            System.out.println("MyInt=>15");
+        } else {
+            System.out.println("ELSE");
+        }
+
+    }
+}
