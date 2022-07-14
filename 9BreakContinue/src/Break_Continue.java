31952154d1a6f073a79005a41db46598a1ef27c6
slavontm <nslava@gmail.com>
2022-07-14 16:59:15 +0300

    upd

--- 9BreakContinue/src/Break_Continue.java
+++ 9BreakContinue/src/Break_Continue.java
@@ -1,19 +1,11 @@
 public class Break_Continue {
     public static void main(String[] args) {
-        int i=0;
-        while(true){
-            if (i==15) {
-
-                break;
+        for(int i=0; i<=15; i++){
+            if (i%2==0) {
+                continue;
             }
-
-
-            System.out.println(i);
-            i++;
-
+            System.out.println("Nechet"+i);
         }
-
-        System.out.println("Exit");
     }
 
 }
