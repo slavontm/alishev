f6dde5286aa5600c511efc64118792d82c31463c
slavon <slavon@slavon.online>
2022-07-21 12:39:20 +0300

    update

--- 10Switch/src/Switch.java
+++ 10Switch/src/Switch.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Switch {
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Vvvedi vozr: ");
+        int age=scanner.nextInt();
+        switch (age) {
+            case 0:
+                System.out.println("Burn");
+                break;
+            case 7:
+                System.out.println("School");
+                break;
+            case 18:
+                System.out.println("Vodka");
+                break;
+            default:
+            System.out.println("Nichego!");
+
+        }
+
+
+    }
+
+
 }
