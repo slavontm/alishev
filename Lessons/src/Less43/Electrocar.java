86a4a96504f0aa25b6dcd73487d4133f69efdbec
slavon <slavon@slavon.online>
2022-07-27 09:55:19 +0300

    sreda

---
+++ Lessons/src/Less43/Electrocar.java
@@ -0,0 +1,13 @@
+package Less43;
+
+public class Electrocar {
+    private int id;
+
+    public Electrocar(int id) {
+        this.id = id;
+    }
+    public void start(){
+
+        System.out.println("Electrocar "+id+" is starting...");
+    }
+}
---
+++ Lessons/src/Less43/Test.java
@@ -0,0 +1,4 @@
+package Less43;
+
+public class Test {
+}
